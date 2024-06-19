263d7ba1dccaacfb9cca1d7e600b2548e8a51979
Adam Johnson <dankswoops@protonmail.com>
2024-06-19 05:39:18 +0000

    Update removal.bat

--- removal.bat
+++ removal.bat
@@ -3,8 +3,8 @@
 rem Set the path
 cd /d "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\custom"
 
-rem Wait for 12 seconds
-timeout /t 12 /nobreak > NUL
+rem Wait for 2 seconds
+timeout /t 2 /nobreak > NUL
 
 rem Delete DoubleClickSwoopsHudUpdater.exe forcefully and quietly
 del /F /Q "DoubleClickSwoopsHudUpdater.exe"
