0a49fa49b9912b39a50bcc4605ce11eaf8c12287
Adam Johnson <dankswoops@protonmail.com>
2024-06-19 20:56:51 +0000

    Add files via upload

---
+++ removal.ps1
@@ -0,0 +1,22 @@
+# SET PATH
+Set-Location -Path "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\custom"
+
+# WAIT 1
+Start-Sleep -Seconds 1
+
+# DELETE OLD UPDATER
+Remove-Item -Path ".\DoubleClickSwoopsHudUpdater.exe" -Force -ErrorAction SilentlyContinue
+
+# UPDATE FOLDER TICON
+$FolderPath = "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\custom\SwoopsHud"
+$IconFile = "folder.ico"
+
+# PATH desktop.ini
+$desktopIniPath = Join-Path -Path $FolderPath -ChildPath "desktop.ini"
+
+# REFRESH FOLDER
+$shell = New-Object -ComObject Shell.Application
+$folder = $shell.Namespace($FolderPath)
+$item = $folder.Self
+$item.InvokeVerb("refresh")
+Write-Host "Updating the SwoopsHud folder icon"
